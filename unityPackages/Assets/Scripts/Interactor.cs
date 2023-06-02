ce6f3b58a57548cc0863bae5a19cbb3f31c3fde0
TQNLP <thiquynh-nhu.pham@edu.esiee.fr>
2023-06-02 10:20:22 +0200

    begin interact event

---
+++ unityPackages/Assets/Scripts/Interactor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactor : MonoBehaviour
+{
+    public int interactDistance = 2;
+    public LayerMask interactableLayerMask = 8;
+    UnityEvent onInteract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactDistance, interactableLayerMask))
+        {
+            if (hit.collider.GetComponent<Interactable>() != false)
+            {
+                onInteract = hit.collider.GetComponent<Interactable>().onInteract;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    onInteract.Invoke();
+                }
+            }
+        }
+    }
+}
